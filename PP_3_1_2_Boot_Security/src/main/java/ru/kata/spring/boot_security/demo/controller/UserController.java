23a48c5b87eae0116d77ce28eef78ab391bd12fc
Sauman012 <ya.js100@yandex.ru>
2024-07-08 10:05:42 +0300

    Add files via upload
    v2

---
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
+
+@Controller
+public class UserController {
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public String getUserForm(Model model) {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        model.addAttribute("user", userService.findByName(name));
+        return "user";
+    }
+}
+
+
+
